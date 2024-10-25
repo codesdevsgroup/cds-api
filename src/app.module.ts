5f0ae36718694d4bff189882769dcbee7bb7ea02
Lucas Campos <bhlucascampos@gmail.com>
2024-10-24 21:38:59 -0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,53 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './services/prisma/prisma.module';
+import { UserModule } from './routes/user/user.module';
+import { AuthModule } from './routes/auth/auth.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { EmailModule } from './services/email/email.module';
+import { GoogleAuthModule } from './routes/google-auth/google-auth.module';
+import { BullModule } from '@nestjs/bullmq';
+import { EmailProcessor } from './services/email/email.processor';
+import { EmailService } from './services/email/email.service';
+import { BlogModule } from './routes/blog/blog.module';
+import { ConfigModule } from './routes/config/config.module';
+import { UploadModule } from './routes/upload/upload.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    EmailModule,
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    EmailModule,
+    GoogleAuthModule,
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'email',
+    }),
+    BlogModule,
+    ConfigModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+    UploadModule,
+  ],
+  providers: [
+    EmailProcessor,
+    EmailService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  controllers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { LogInterceptor } from './interceptors/log.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cors from 'cors';
+import './logger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cors({
+      origin: ['https://liberty.codesdevs.com.br', 'http://localhost:4200'],
+      methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'enctype'],
+      credentials: true,
+      optionsSuccessStatus: 200, // For legacy browser support
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new LogInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Documentação com Swagger - CodesDevs')
+    .setDescription(
+      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend.',
+    )
+    .setVersion('1.0')
+    .addTag('user')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3005);
+}
+
+bootstrap();
